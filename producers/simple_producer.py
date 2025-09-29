9f8618aefa959890de6098256ebb6685dd6c28be
binseoyun <sunnybin04@gmail.com>
2025-09-29 18:18:28 +0900

    ch5.3 작성

--- producers/simple_producer.py
+++ producers/simple_producer.py
@@ -3,9 +3,10 @@ from confluent_kafka import Producer
 import sys
 import time
 
+#접속할 카프카 브로커의 주소 목록
 BROKER_LST = 'kafka01:9092,kafka02:9092,kafka03:9092'
 
-##
+
 class SimpleProducer:
 
     def __init__(self, topic, duration=None):
@@ -13,7 +14,7 @@ class SimpleProducer:
         self.duration = duration if duration is not None else 60
         self.conf = {'bootstrap.servers': BROKER_LST}
 
-        self.producer = Producer(self.conf)
+        self.producer = Producer(self.conf) #produce 할 시작점
 
     # Optional per-message delivery callback (triggered by poll() or flush())
     # when a message has been successfully delivered or permanently
@@ -27,16 +28,18 @@ class SimpleProducer:
 
     def produce(self):
         cnt = 0
-        while cnt < self.duration:
+        while cnt < self.duration: #정해진 시간 동안 반복
             try:
                 # Produce line (without newline)
                 self.producer.produce(
                     topic=self.topic,
                     key=str(cnt),
                     value=f'hello world: {cnt}',
-                    on_delivery=self.delivery_callback)
+                    on_delivery=self.delivery_callback #전송결과가 오면 이 함수 호출
+                )
 
             except BufferError:
+                #프로듀서 내부 버퍼가 꽉 찼을 때 예외 처리
                 sys.stderr.write('%% Local producer queue is full (%d messages awaiting delivery): try again\n' %
                                  len(self.producer))
 
@@ -50,7 +53,7 @@ class SimpleProducer:
 
         # Wait until all messages have been delivered
         sys.stderr.write('%% Waiting for %d deliveries\n' % len(self.producer))
-        self.producer.flush()
+        self.producer.flush() #프로그램이 종료되기 전에 producer에 남아있는 데이터를 broker로
 
 
 if __name__ == '__main__':

a42e087d77c1bb84b8886d38956522a0a80b5dca
binseoyun <sunnybin04@gmail.com>
2025-09-29 18:37:36 +0900

    민감 정보 관리

--- deploy/after_install.sh
+++ deploy/after_install.sh
@@ -1,3 +1,4 @@
 #hook에 의해 실행
 source /src/kafka_venv/bin/activate #python 가상환경 실행
+python3 /src/kafka-producer/deploy/replace_secret.py
 pip3 install -r /src/kafka-producer/requirements.txt #EC2 상에도 동일한 라이브러리 설치
